b67b4483b39ba66b647a33ea30699aad4e6a644a
Coihue555 <kevlar555@gmail.com>
2022-03-07 09:22:17 -0300

    details screen

--- peliculas/lib/screens/details_screen.dart
+++ peliculas/lib/screens/details_screen.dart
@@ -8,19 +8,91 @@ class DetailsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detalles de la pelicula'),
-        elevation: 0,
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search_outlined))
+      body: CustomScrollView(
+        slivers: [
+          const _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ])
+          )
         ],
-      ),
-      body: Container(
-        child: const Center(
-          child: Text('Details Screen'),
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.green,
+      expandedHeight: 200,
+      floating: true,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle( fontSize: 16, ),            
+            ),
+
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top:20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          
+          const SizedBox(width: 20,),
+          
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('movie.title', style: Theme.of(context).textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2,),
+              Text('movie.originalTitle', style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis,),
+
+              Row(
+                children: const [
+                  Icon(Icons.star_outline, size: 15, color: Colors.grey ),
+                ],
+              )
+            ],
+          )
+        ]),
     );
   }
 }
--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -8,7 +8,6 @@ class MovieSlider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 250,
-      color: Colors.red,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
@@ -17,6 +16,9 @@ class MovieSlider extends StatelessWidget {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
+        
+        const SizedBox(height:5),
+
         Expanded(
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -33,19 +35,31 @@ class _MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 130,
-      height: 190,
-      color: Colors.green,
+      height: 260,
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
-        children: const [
-          FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'),
-            image: NetworkImage('https://via.placeholder.com/300x400'),
-            width: 130,
-            height: 190,
-            fit: BoxFit.cover,
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          Text('Star Wars'),
+
+          const SizedBox(height:5),
+
+          const Text('Star Wars',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
